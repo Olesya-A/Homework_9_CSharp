a3f84ca069cd307d7563f4795b63edce64a5e8b2
Olesya <lesusha1@yandex.ru>
2022-10-04 02:40:16 +0300

    add Task_64

---
+++ Task_64/Program.cs
@@ -0,0 +1,24 @@
+﻿// 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5. -> "1, 2, 3, 4, 5"
+// M = 4; N = 8. -> "4, 6, 7, 8"
+
+Console.Write("Введите число M: ");
+int M = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine()!);
+
+int min = M;
+int max = N;
+if (N < M)
+{
+    min = N;
+    max = M;
+}
+
+Console.Write(NumeralRow(min, max));
+
+string NumeralRow(int min, int max)
+{
+    if (max == min) return $"{min}";
+    else return NumeralRow(min, max - 1) + ", " + max;
+}
